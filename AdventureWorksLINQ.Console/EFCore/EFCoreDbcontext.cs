839472e5a9357de5eb8004f8a399d799e3d1c525
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-03 19:58:17 +0330

    Transform N-->N Relationship into 1-->N

---
+++ AdventureWorksLINQ.Console/EFCore/EFCoreDbcontext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AdventureWorksLINQ.Console.EFCore
+{
+    public class EFCoreDbcontext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+        public EFCoreDbcontext(DbContextOptions<EFCoreDbcontext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+            .HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+            modelBuilder.Entity<StudentCourse>()
+            .HasOne(sc => sc.Student)
+            .WithMany(s => s.StudentCourses)
+            .HasForeignKey(sc => sc.StudentId);
+
+            modelBuilder.Entity<StudentCourse>()
+            .HasOne(sc => sc.Course)
+            .WithMany(c => c.StudentCourses)
+            .HasForeignKey(sc => sc.CourseId);
+        }
+    }
+}
