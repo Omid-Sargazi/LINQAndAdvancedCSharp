1341d5cb25e0584e1522dd290b61a42214fb4f9f
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-13 13:45:28 +0330

    SelectMany in Lists for flattened them

--- LinqExamples/Exmaple1/ExampleOfLinq.cs
+++ LinqExamples/Exmaple1/ExampleOfLinq.cs
@@ -6,7 +6,18 @@ namespace LinqExamples.Exmaple1
         {
             var numbers = new List<int> { 1, 22, 3, 4, 5, 6, 7, 8, 99 };
             var doubled = numbers.Select(n => n * 2);
-            Console.WriteLine(string.Join(",",doubled));
+            Console.WriteLine(string.Join(",", doubled));
+
+
+            var listOfLists = new List<List<int>>
+            {
+                new List<int>{1,2,3},
+                new List<int>{4,5,6},
+                new List<int>{7,8,9}
+            };
+
+            var flattened = listOfLists.SelectMany(x => x);
+            Console.WriteLine(string.Join(',', flattened));
         }
     }
 }
