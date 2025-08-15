76e514c683119eac4111f72f908434854c681fa5
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-15 19:22:04 +0330

    ValueConverter AES

--- SecureEfCoreDemo/Models/User.cs
+++ SecureEfCoreDemo/Models/User.cs
@@ -1,4 +1,6 @@
+using System.Security.Cryptography;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace SecureEfCoreDemo.Models
 {
@@ -41,17 +43,51 @@ namespace SecureEfCoreDemo.Models
     public class PasswordHasher
     {
         public static void CreatePasswordHash(string password, out byte[] hash, out byte[] salt)
-    {
-        using var hmac = new System.Security.Cryptography.HMACSHA512();
-        salt = hmac.Key;
-        hash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        {
+            using var hmac = new System.Security.Cryptography.HMACSHA512();
+            salt = hmac.Key;
+            hash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] hash, byte[] salt)
+        {
+            using var hmac = new System.Security.Cryptography.HMACSHA512(salt);
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(hash);
+        }
     }
 
-    public static bool VerifyPasswordHash(string password, byte[] hash, byte[] salt)
+    public class EncryptionConverter
     {
-        using var hmac = new System.Security.Cryptography.HMACSHA512(salt);
-        var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-        return computedHash.SequenceEqual(hash);
+        public EncryptionConverter(string key, ConverterMappingHints? mappingHints = null)
+        : base(
+            v => Encrypt(v, key),
+            v => Decrypt(v, key),
+            mappingHints)
+    { }
+
+    private static string Encrypt(string plain, string key)
+    {
+        using var aes = Aes.Create();
+        aes.Key = Convert.FromBase64String(key);
+        aes.GenerateIV();
+        using var encryptor = aes.CreateEncryptor();
+        var plainBytes = System.Text.Encoding.UTF8.GetBytes(plain);
+        var cipherBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+        return Convert.ToBase64String(aes.IV.Concat(cipherBytes).ToArray());
+    }
+
+    private static string Decrypt(string cipherText, string key)
+    {
+        var fullBytes = Convert.FromBase64String(cipherText);
+        using var aes = Aes.Create();
+        aes.Key = Convert.FromBase64String(key);
+        var iv = fullBytes.Take(16).ToArray();
+        var cipherBytes = fullBytes.Skip(16).ToArray();
+        aes.IV = iv;
+        using var decryptor = aes.CreateDecryptor();
+        var plainBytes = decryptor.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+        return System.Text.Encoding.UTF8.GetString(plainBytes);
     }
     }
 }
