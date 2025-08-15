59caf84496831f8a7b3939af293949db14904e10
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-15 19:19:39 +0330

    Create Use and Recode class and A DbContext

---
+++ SecureEfCoreDemo/Models/User.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SecureEfCoreDemo.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public byte[] PasswordHash { get; set; } = null!;
+        public byte[] PasswordSalt { get; set; } = null!;
+        public string NationalId { get; set; } = null!;
+    }
+
+    public class Record
+    {
+        public int Id { get; set; }
+        public string Data { get; set; } = null!;
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+
+    public class AppDbContext : DbContext
+    {
+        private readonly int _currentUserId;
+         public AppDbContext(DbContextOptions<AppDbContext> options, IHttpContextAccessor accessor)
+        : base(options)
+    {
+        // فرض: کاربر لاگین شده، UserId توی Claims ذخیره شده
+        _currentUserId = int.TryParse(accessor.HttpContext?.User?.FindFirst("UserId")?.Value, out var id) ? id : 0;
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Record> Records { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Record>().HasQueryFilter(r => r.UserId == _currentUserId);
+    }
+    }
+}
