c3ee17c38fa27823171d256414516e925e4dbce4
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-17 22:52:04 +0330

    Selection Sort In C Sharp

--- SortingInCSharp/Program.cs
+++ SortingInCSharp/Program.cs
@@ -6,4 +6,5 @@ Console.WriteLine("Hello, World!");
 Reflection.Run();
 
 int[] arr = new int[] { 10, 20, 30, 70, -70, -10, 1, 2 };
-BubbleSorting.Run(arr);
+// BubbleSorting.Run(arr);
+SelectioSort.Run(arr);
--- SortingInCSharp/Sorting.cs
+++ SortingInCSharp/Sorting.cs
@@ -7,7 +7,7 @@ namespace SortingInCSharp
             for (int i = arr.Length - 1; i >= 0; i--)
             {
                 bool swapped = false;
-                for (int j = 0; j <= i-1; j++)
+                for (int j = 0; j <= i - 1; j++)
                 {
                     if (arr[j] > arr[j + 1])
                     {
@@ -21,9 +21,33 @@ namespace SortingInCSharp
                 }
             }
 
-           
-                Console.WriteLine(String.Join(",", arr));
-            
+
+            Console.WriteLine(String.Join(",", arr));
+
+        }
+    }
+
+    public class SelectioSort
+    {
+        public static void Run(int[] arr)
+        {
+            for (int i = 0; i <= arr.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i; j <= arr.Length - 1; j++)
+                {
+                    if (arr[j] >  arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    (arr[minIndex], arr[i]) = (arr[i], arr[minIndex]);
+                }
+            }
+
+            Console.WriteLine(string.Join(",", arr));
         }
     }
 }
