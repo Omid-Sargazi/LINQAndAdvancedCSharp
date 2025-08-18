bf87a85ec237d0b478392a3cfb98155f25680367
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-18 20:14:36 +0330

    Create Order Service

---
+++ MicroservicesDay2/OrdersService/Controllers/OrdersController.cs
@@ -0,0 +1,16 @@
+namespace OrdersService.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private static List<Order> items = new();
+        private static int id = 0;
+        [HttpGet] public IEnumerable<Order> GetAll() => items;
+        [HttpGet("{id}")] public ActionResult<Order> Get(int id) => items.FirstOrDefault(x => x.Id == id) ?? NotFound();
+        [HttpPost] public Order Create(Order dto) { dto.Id = ++id; items.Add(dto); return dto; }
+    }
+
+
+}
