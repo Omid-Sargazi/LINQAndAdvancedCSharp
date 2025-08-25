902827e761d626493680e76bff6e06ead5ef50b9
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-25 20:21:44 +0330

    Sales Representatives in the United States

--- AdventureWorksLINQ.Console/Product/SqlBook.cs
+++ AdventureWorksLINQ.Console/Product/SqlBook.cs
@@ -38,6 +38,46 @@ namespace AdventureWorksLINQ.Console.Product
                 System.Console.WriteLine(item);
             }
 
+            var query = (from e in db.Employees
+
+                         join p in db.BusinessEntities
+                         on e.BusinessEntityId equals p.BusinessEntityId
+                         where EF.Functions.Like(e.JobTitle, "Sales Representative%")
+                         select new
+                         {
+                             e.HireDate,
+                             e.BusinessEntity.FirstName,
+                             e.BusinessEntity.LastName
+                         }
+            );
+
+
+            var countries = db.Employees
+            .Include(e => e.BusinessEntity)
+            .Include(e => e.BusinessEntity.BusinessEntity.BusinessEntityAddresses)
+            .ThenInclude(bea => bea.Address)
+            .ThenInclude(a => a.StateProvince)
+            .Where(e => EF.Functions.Like(e.JobTitle, "Sales Representative%")
+                && e.BusinessEntity.BusinessEntity.BusinessEntityAddresses.Any(bea => bea.Address.StateProvince.CountryRegionCode == "US")
+            ).Select(e => new
+            {
+                e.BusinessEntity.FirstName,
+                e.BusinessEntity.LastName,
+                postalCode = e.BusinessEntity.BusinessEntity.BusinessEntityAddresses.First().Address.PostalCode,
+                CountryRegionCode = e.BusinessEntity.BusinessEntity.BusinessEntityAddresses.First().Address.StateProvince.CountryRegionCode
+            }).ToList();
+
+
+            foreach (var item in countries)
+            {
+                System.Console.WriteLine(item+"Contries");
+            }
+            
+
+            
+
+
+
             
         }
     }
