ac959e3d1efd1b62a65da00353e6622959ecfaab
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-28 10:12:58 +0330

    Insertion Sort

--- AlgorithemInCSharp/Sorting/Sorting.cs
+++ AlgorithemInCSharp/Sorting/Sorting.cs
@@ -40,7 +40,25 @@ namespace AlgorithemInCSharp.Sorting
 
             Console.WriteLine(string.Join(",", arr) + " Seelction");
         }
+
+        public static void Insertion(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int j = i - 1;
+                int current = arr[i];
+                while (j >= 0 && arr[j] > current)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = current;
+            }
+
+            Console.WriteLine(string.Join(",", arr));
+        }
         
-        
+
     }
 }
