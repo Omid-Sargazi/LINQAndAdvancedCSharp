11d87aad03ba97be64d01babc506f650b51415c2
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-28 10:35:08 +0330

    Merege Sorting In C#

---
+++ AlgorithemInCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlgorithemInCSharp.Sorting;
+
+Console.WriteLine("Hello, World!");
+int[] arr1 = new int[] { 1 };
+int[] arr2 = new int[] { -1 };
+int[] arr3 = new int[] { 0, 180, -80, 70, -70, 100, -100 };
+
+// Sorting.Bubble(arr3);
+
+// Sorting.Selection(arr3);
+// Sorting.Insertion(arr3);
+
+Sorting.MergeSorting(arr3);
--- AlgorithemInCSharp/Sorting/Sorting.cs
+++ AlgorithemInCSharp/Sorting/Sorting.cs
@@ -58,6 +58,70 @@ namespace AlgorithemInCSharp.Sorting
 
             Console.WriteLine(string.Join(",", arr));
         }
+
+
+        public static void MergeSorting(int[] arr)
+        {
+            if (arr.Length <= 1) return;
+            int n = arr.Length;
+            int mid = n / 2;
+
+            int[] left = new int[mid];
+            int[] right = new int[n - mid];
+            Array.Copy(arr, 0, left, 0, mid);
+            Array.Copy(arr, mid, right, 0, right.Length);
+
+            Console.WriteLine(string.Join(",", left) + "  left  " + mid);
+            Console.WriteLine(string.Join(",", right) + "  right");
+
+
+            MergeSorting(left);
+            MergeSorting(right);
+            Merge(arr, left, right);
+        }
+
+        private static void Merge(int[] result, int[] left, int[] right)
+        {
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
+
+            int n1 = left.Length;
+            int n2 = right.Length;
+
+            while (p1 < n1 && p2 < n2)
+            {
+                if (left[p1] < right[p2])
+                {
+                    result[p3] = left[p1];
+                    p1++;
+                }
+
+                else
+                {
+                    result[p3] = right[p2];
+                    p2++;
+                }
+
+                p3++;
+            }
+
+            while (p1 < n1)
+            {
+                result[p3] = left[p1];
+                p3++;
+                p1++;
+            }
+
+            while (p2 < n2)
+            {
+                result[p3] = right[p2];
+                p2++;
+                p3++;
+            }
+
+            Console.WriteLine(string.Join(",", result)+ "  Merge Sorting");
+        } 
         
 
     }
