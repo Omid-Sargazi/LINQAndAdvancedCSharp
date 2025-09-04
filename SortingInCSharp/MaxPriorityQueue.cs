32ab8c1e1e3fcdc1d4e958c39be5020d1eecc2f7
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-04 08:50:06 +0330

    Insert in MaxPriorityQueue

--- SortingInCSharp/MaxPriorityQueue.cs
+++ SortingInCSharp/MaxPriorityQueue.cs
@@ -1,18 +1,42 @@
+using System.Security.AccessControl;
+
 namespace SortingInCSharp
 {
     public class MaxPriorityQueue
     {
         private List<int> heap = new List<int>();
 
-        public int Count => heap.Count();
+        public int Count => heap.Count;
         public bool IsEmpty => heap.Count == 0;
 
-        public MaxPriorityQueue() { }
-
-        public MaxPriorityQueue(IEnumerable<int> items)
+        public void Insert(int value)
         {
-            heap = new List<int>(items);
-            BuildHeap();
+            heap.Add(value);
+            SiftUp(heap.Count - 1);
+
+        }
+
+        private void SiftUp(int index)
+        {
+            int current = index;
+            while (current > 0)
+            {
+                int parent = (current - 1) / 2;
+                if (heap[parent] < heap[current])
+                {
+                    Swap(parent, current);
+                    current = parent;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Swap(int i, int j)
+        {
+            (heap[i], heap[j]) = (heap[j], heap[i]);
         }
 
         public override string ToString()
