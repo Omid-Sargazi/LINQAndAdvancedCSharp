45c041d2c47e1a650f214e00376eed660edbbff3
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-26 18:40:24 +0330

    LINQ

--- LinqExamples/EnumerableExample/Enumerable.cs
+++ LinqExamples/EnumerableExample/Enumerable.cs
@@ -38,4 +38,17 @@ namespace LinqExamples.EnumerableExample
             var evenNumbers = nums.Where(x => x % 2 == 0);
         }
     }
+
+    public class MyCollection
+    {
+        private string[] data = new string[5];
+
+        public string this[int index]
+        {
+            get { return data[index]; }
+            set { data[index] = value; }
+        }
+    }
+    
+    
 }
