35fee38de0c79f606dfade877a37391e6aa49eb2
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-27 07:10:39 +0330

    You are given an array of prices where prices[i] is the price of a given stock on an ith day.

--- AlgorithemInCSharp/LeetcodeExample/LeetCodeProblems.cs
+++ AlgorithemInCSharp/LeetcodeExample/LeetCodeProblems.cs
@@ -228,6 +228,21 @@ namespace AlgorithemInCSharp.LeetcodeExample
             return new int[] { 0 };
         }
 
+        public static int BestTimeBuySellStock(int[] prices)
+        {
+            if (prices == null || prices.Length < 2) return 0;
+            int maxSofar = 0;
+            int result = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                int diff = prices[i] - prices[i - 1];
+                maxSofar = Math.Max(0, maxSofar + diff);
+                result = Math.Max(result, maxSofar);
+            }
+
+            return result;
+        }
         
     }
 }
