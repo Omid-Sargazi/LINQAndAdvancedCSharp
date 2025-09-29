70a94e99bfe999456a17503ea6cbc7c558d2c3c3
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-29 09:35:55 +0330

    .

--- LinqExamples/LiveCoding/IsPalindrome.cs
+++ LinqExamples/LiveCoding/IsPalindrome.cs
@@ -1,3 +1,5 @@
+using System.Data.Common;
+
 namespace LinqExamples.LiveCoding
 {
     public class IsPalindrome
@@ -34,6 +36,65 @@ namespace LinqExamples.LiveCoding
 
             return '\0';
 
+        }
+
+        public static bool AreAnagrams(string s1, string s2)
+        {
+            if (s1.Length != s2.Length) return false;
+
+            var charCount = new int[256];
+
+            foreach (char c in s1)
+            {
+                charCount[c]++;
+            }
+
+
+            foreach (char c in s2)
+            {
+                charCount[c]--;
+                if (charCount[c] < 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static int MostFrequent(int[] nums)
+        {
+            var frequency = new Dictionary<int, int>();
+            int maxCount = 0, result = nums[0];
+
+            foreach (var num in nums)
+            {
+                frequency[num] = frequency.GetValueOrDefault(num, 0) + 1;
+                if (frequency[num] > maxCount)
+                {
+                    maxCount = frequency[num];
+                    result = num;
+                }
+            }
+
+            return result;
+        }
+
+        public static int MostFrequent2(int[] nums)
+        {
+            var frequency = new Dictionary<int, int>();
+            int maxCount = 0, result = nums[0];
+
+            foreach (var num in nums)
+            {
+                frequency[num] = frequency.GetValueOrDefault(num, 0) + 1;
+                if (frequency[num] > maxCount)
+                {
+                    maxCount = frequency[num];
+                    return num;
+                }
+            }
+            return result;
         }
     }
 }
