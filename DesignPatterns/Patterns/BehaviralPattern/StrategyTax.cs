4713e1c1479bb854c8281035c8b32e6c3e65428b
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-14 17:57:07 +0330

    Strategy Pattern for calculate Tax in some country

---
+++ DesignPatterns/Patterns/BehaviralPattern/StrategyTax.cs
@@ -0,0 +1,103 @@
+namespace Patterns.BehaviralPattern
+{
+    public enum Country
+    {
+        USA,
+        German,
+        UEA,
+        England,
+        China,
+        Russia,
+
+    };
+
+    public interface ITaxStartegy
+    {
+        decimal Calculate(decimal price);
+    }
+
+    public class USATax : ITaxStartegy
+    {
+        public decimal Calculate(decimal price)
+        {
+            return price + price * 0.1m + .9m;
+        }
+    }
+
+    public class RussiaTax : ITaxStartegy
+    {
+        public decimal Calculate(decimal price)
+        {
+            return price + price * .3m + 0.8m;
+        }
+    }
+    public class UEATax : ITaxStartegy
+    {
+        public decimal Calculate(decimal price)
+        {
+            return price + price * 0.7m + .4m;
+        }
+    }
+
+    public class ChinaTax : ITaxStartegy
+    {
+        public decimal Calculate(decimal price)
+        {
+            return price + price * 1.1m;
+        }
+    }
+
+    public class GermanTax : ITaxStartegy
+    {
+        public decimal Calculate(decimal price)
+        {
+            return price + price * 1.8m;
+        }
+    }
+
+    public class EnglandTax : ITaxStartegy
+    {
+        public decimal Calculate(decimal price)
+        {
+            return price + price * 1.8m;
+        }
+    }
+
+
+    public class TaxStrategyFactory
+    {
+        public ITaxStartegy GetTaxStrtegy(Country country)
+        {
+            switch (country)
+            {
+                case Country.China:
+                    return new ChinaTax();
+                case Country.England:
+                    return new EnglandTax();
+                case Country.UEA:
+                    return new EnglandTax();
+                case Country.German:
+                    return new GermanTax();
+                case Country.Russia:
+                    return new RussiaTax();
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+
+    public class ClientTax
+    {
+        protected readonly  ITaxStartegy _taxStartegy;
+        public ClientTax(ITaxStartegy taxStartegy)
+        {
+            _taxStartegy = taxStartegy;
+        }
+        public decimal CalTax(decimal price)
+        {
+            var res = _taxStartegy.Calculate(price);
+            return res;
+            
+        }
+    }
+}
