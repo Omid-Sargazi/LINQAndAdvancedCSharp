92d894b8cdcea49fb81193140e06cfbc3c8abbae
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-14 17:27:38 +0330

    Usage of Strategy pattern in TAX

--- AlgorithemInCSharp/Program.cs
+++ AlgorithemInCSharp/Program.cs
@@ -2,6 +2,7 @@
 using AlgorithemInCSharp.Patterns;
 using AlgorithemInCSharp.Patterns.BehavioralDesignPattern;
 using AlgorithemInCSharp.Reflections;
+using AlgorithemInCSharp.RpositoryPattern;
 
 // Example.Run();
 
@@ -13,6 +14,13 @@ using AlgorithemInCSharp.Reflections;
 // await NotificationClient.Run();
 // LSubstringWithoutRepeatingCharacters.Run("Omidss");
 // ClientLits.Run();
-Deconstructt d1 = new Deconstructt("Omid", 42);
-var (name, age) = d1;
-Console.WriteLine(name + age);
+// Deconstructt d1 = new Deconstructt("Omid", 42);
+// var (name, age) = d1;
+// Console.WriteLine(name + age);
+
+var factory = new TaxStrategyFactory();
+var germanTax = factory.GetTaxStrategy(Country.German);
+
+ClientTax cli = new ClientTax(germanTax);
+
+Console.WriteLine($"{cli.CalTax(45)}");
