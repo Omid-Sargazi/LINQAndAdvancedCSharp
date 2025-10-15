e241a75dde3864a6ae1c61cb1ff6a1c5e74e6d18
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-15 21:50:57 +0330

    SemaphorSlim for limiting executing

--- DesignPatterns/Patterns/TaskProblem/Tasks.cs
+++ DesignPatterns/Patterns/TaskProblem/Tasks.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+using System.Formats.Asn1;
 using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 
@@ -15,28 +17,115 @@ namespace Patterns.TaskProblem
     public interface ITask
     {
         TaskPriority Priority { get; }
+        public bool IsRunning { get; }
+        public bool IsCompleted { get; }
         Task ExecuteAsync();
     }
 
     public class Tasks : ITask
     {
         public TaskPriority Priority { get; }
+        public string Name { get; }
 
-        public Tasks(TaskPriority taskPriority)
+        public bool IsRunning { get;}
+
+        public bool IsCompleted { get; }
+
+        public Tasks(string name,TaskPriority taskPriority)
         {
             Priority = taskPriority;
+            Name = name;
         }
 
-        public Task ExecuteAsync()
+        public async Task ExecuteAsync()
         {
+            Console.WriteLine($"[START] {Priority}");
             Console.WriteLine($"Running task with priority: {Priority}");
-            return Task.CompletedTask;
+            await Task.Delay(1000);
+            Console.WriteLine($"[END] {Priority}");
+            // return Task.CompletedTask;
+        }
+        
+        public async Task ExecuteAsync(CancellationToken cancellationToken = default)
+        {
+            Console.WriteLine($"[START] {Name} ({Priority}) on Thread {Environment.CurrentManagedThreadId}");
+
+            await Task.Delay(700 + (int)Priority * 200, cancellationToken);
+
+            try
+            {
+                  if (Priority == TaskPriority.Low && new Random().Next(0, 4) == 0)
+            {
+                throw new InvalidOperationException($"Simulated error in {Name}");
+            }
+            Console.WriteLine($"[END]   {Name} ({Priority})");
+            }
+            catch (OperationCanceledException)
+            {
+                
+                 Console.WriteLine($"[CANCELED] {Name}");
+                throw;
+            }
+
         }
     }
 
     public class TaskQueue
     {
         private List<ITask> _tasks = new List<ITask>();
+
+        public async Task RunWithConcurrencyLimitAsync(int maxConcurrency, CancellationToken cancellationToken=default)
+        {
+            if (maxConcurrency <= 0) throw new ArgumentOutOfRangeException(nameof(maxConcurrency));
+
+            var sem = new SemaphoreSlim(maxConcurrency);
+            var ordered = _tasks.OrderByDescending(t => t.Priority).ToList();
+
+            var runningTasks = new List<Task>();
+
+            foreach(var itask in ordered)
+            {
+                await sem.WaitAsync(cancellationToken);
+
+                var wrapper = Task.Run(async () =>
+                {
+                    try
+                    {
+                        await itask.ExecuteAsync();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine($"[EX] {ex.Message}");
+                        throw;
+                    }
+                    finally
+
+                    {
+                        sem.Release();
+                    }
+
+
+                }, cancellationToken);
+
+                runningTasks.Add(wrapper);
+
+                try
+                {
+                    await Task.WhenAll(runningTasks);
+                }
+                catch (System.Exception)
+                {
+
+                    throw;
+                }
+                finally
+
+                                {
+                    _tasks.Clear();
+                }
+            }
+        }
 
         public TaskQueue() { }
 
@@ -63,11 +152,22 @@ namespace Patterns.TaskProblem
         {
             var resCritical = _tasks.FirstOrDefault(t => t.Priority == TaskPriority.Critical);
             var res = _tasks.OrderByDescending(t => t.Priority);
-            
-            foreach(var task in res)
+
+            foreach (var task in res)
             {
                 await task.ExecuteAsync();
             }
+        }
+        
+        public async Task RunInBatchesAsync()
+        {
+            var sortedTask = _tasks.OrderByDescending(t => t.Priority).ToList();
+            for (int i = 0; i < sortedTask.Count; i += 3)
+            {
+                var res = sortedTask.Skip(i).Take(3);
+                await Task.WhenAll(res.Select(t => t.ExecuteAsync()));
+            }
+            _tasks.Clear();
         }
     }
 
@@ -75,15 +175,30 @@ namespace Patterns.TaskProblem
     {
         public static async Task Run()
         {
-            TaskQueue task = new TaskQueue();
-            task.AddTask(new Tasks(TaskPriority.High));
-            task.AddTask(new Tasks(TaskPriority.Critical));
-            task.AddTask(new Tasks(TaskPriority.Normal));
-            task.AddTask(new Tasks(TaskPriority.Critical));
-            task.AddTask(new Tasks(TaskPriority.High));
+            // TaskQueue task = new TaskQueue();
+            // task.AddTask(new Tasks("T1",TaskPriority.High));
+            // task.AddTask(new Tasks(TaskPriority.Critical));
+            // task.AddTask(new Tasks(TaskPriority.Normal));
+            // task.AddTask(new Tasks(TaskPriority.Critical));
+            // task.AddTask(new Tasks(TaskPriority.High));
 
             // await task.RunAllAsync();
-            await task.RunOnPriorityAsync();
+            // await task.RunOnPriorityAsync();
+
+            // await task.RunInBatchesAsync();
+
+            var q = new TaskQueue();
+            q.AddTask(new Tasks("T1", TaskPriority.High));
+            q.AddTask(new Tasks("T2", TaskPriority.Critical));
+            q.AddTask(new Tasks("T3", TaskPriority.Normal));
+            q.AddTask(new Tasks("T4", TaskPriority.Critical));
+            q.AddTask(new Tasks("T5", TaskPriority.High));
+            q.AddTask(new Tasks("T6", TaskPriority.Low));
+
+            var cts = new CancellationTokenSource();
+
+            await q.RunWithConcurrencyLimitAsync(3, cts.Token);
+            Console.WriteLine("All DOne");
         }
     }
 }
