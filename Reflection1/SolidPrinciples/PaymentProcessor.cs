0fb3d339472ad168c6f5f30435db033000366aab
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-26 22:01:48 +0330

    Implement SingleResponsibility Principle

--- Reflection1/SolidPrinciples/PaymentProcessor.cs
+++ Reflection1/SolidPrinciples/PaymentProcessor.cs
@@ -22,10 +22,49 @@ namespace Reflection1.SolidPrinciples
         {
             Console.WriteLine("Generating Payment Report...");
         }
-        
+
         public void SaveToDatabase()
         {
             Console.WriteLine("Saving Payment to DB...");
         }
+    }
+
+    public class GoodPaymentProcessor
+    {
+        private readonly PaymentService _paymentService;
+        private readonly ReportService _reportService;
+        public GoodPaymentProcessor()
+        {
+            _paymentService = new PaymentService();
+            _reportService = new ReportService();
+        }
+
+        public void Process()
+        {
+            
+        }
+    }
+
+    public class PaymentService
+    {
+        public void Pay(string paymentType)
+        {
+            if (paymentType == "CreditType")
+                Console.WriteLine("Processing credit card payment...");
+            else if(paymentType=="PayPal")
+            {
+                Console.WriteLine("Processing PayPal payment...");
+            }
+        }
+    }
+
+    public class ReportService
+    {
+        public void Generate() => Console.WriteLine("Generating Payment Report...");
+    }
+
+    public class DatabaseService
+    {
+        public void Save() => Console.WriteLine("Saving Payment to DB...");
     }
 }
