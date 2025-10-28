814e11beb3af3360072cc4952ed2ba428acdecaf
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-28 22:48:45 +0330

    git commit -m "Solve Problem 2: Filter names starting with 'A' and sort alphabetically
    - Filter names using Where with StartsWith (case insensitive)
    - Sort alphabetically using OrderBy
    - Input: [Ali, Mohammad, Ahmad, Sara, Amir, Fatemeh, Armin]
    - Output: [Ahmad, Ali, Amir, Armin]"

--- LinqExamples/Problems/LinqProblem1.cs
+++ LinqExamples/Problems/LinqProblem1.cs
@@ -7,6 +7,16 @@ namespace LinqExamples.Problems
             int[] nums = { 5, 5, 4, 7, 8, 9, 4, 6, 2, 37, 4 };
             var res1 = nums.Where(n => n % 2 == 0).OrderByDescending(n => n);
             Console.WriteLine($"{string.Join(",", res1)}");
+
+
+            List<string> students = new List<string>
+            {
+                "Ali", "Mohammad", "Ahmad", "Sara", "Amir", "Fatemeh", "Armin"
+            };
+
+            var res2 = students.Where(s => s.StartsWith("A")).OrderBy(s => s);
+            Console.WriteLine($"{string.Join(", " , res2)}");
+
         }
     }
 }
