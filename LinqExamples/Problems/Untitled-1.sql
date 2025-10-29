e0dad4c0afddb13713389fd0cc852908de3f4c33
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-29 12:52:20 +0330

    Member of the library

--- LinqExamples/Problems/Untitled-1.sql
+++ LinqExamples/Problems/Untitled-1.sql
@@ -1,6 +1,15 @@
-CREATE TABLE Category(
+-- CREATE TABLE Category(
+--     Id INT IDENTITY(1,1) PRIMARY KEY,
+--     Name NVARCHAR(100),
+--     ParentId INT null,
+--     FOREIGN KEY(ParentId) REFERENCES Category(Id)
+-- );
+
+CREATE TABLE Member
+(
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    Name NVARCHAR(100),
-    ParentId INT null,
-    FOREIGN KEY(ParentId) REFERENCES Category(Id)
+    FullName NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(100),
+    Phone NVARCHAR(50),
+    JoinDate DATETIME DEFAULT GETDATE(),
 );
