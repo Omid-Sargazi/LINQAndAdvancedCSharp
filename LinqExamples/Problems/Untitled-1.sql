84b4b5a6b136166bef1e12431041da70088328ae
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-29 12:57:08 +0330

    Book that a category and has a foreign key

--- LinqExamples/Problems/Untitled-1.sql
+++ LinqExamples/Problems/Untitled-1.sql
@@ -5,11 +5,21 @@
 --     FOREIGN KEY(ParentId) REFERENCES Category(Id)
 -- );
 
-CREATE TABLE Member
-(
+-- CREATE TABLE Member
+-- (
+--     Id INT IDENTITY(1,1) PRIMARY KEY,
+--     FullName NVARCHAR(100) NOT NULL,
+--     Email NVARCHAR(100),
+--     Phone NVARCHAR(50),
+--     JoinDate DATETIME DEFAULT GETDATE(),
+-- );
+
+CREATE TABLE Book(
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    FullName NVARCHAR(100) NOT NULL,
-    Email NVARCHAR(100),
-    Phone NVARCHAR(50),
-    JoinDate DATETIME DEFAULT GETDATE(),
+    Title NVARCHAR(200) NOT NULL,
+    Author NVARCHAR(100),
+    ISBN NVARCHAR(50),
+    PublishYear INT,
+    CategoryId INT null,
+    FOREIGN KEY(CategoryId) REFERENCES Category(Id)
 );
