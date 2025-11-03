0c3c0ff18a8aa3d145d3a0bfacd8728bae6c0cb7
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-03 17:13:09 +0330

    Merge Two Array

--- StandardProblems/Problems/Problem1.cs
+++ StandardProblems/Problems/Problem1.cs
@@ -6,9 +6,15 @@ namespace StandardProblems.Problems
         {
             // var res = SumTwoNumsAnotherWay(new int[] { 1, 2, 3, 4, 6, 8, 9, 11 }, 10);
             // Console.WriteLine($"Indices: {res[0]}, {res[1]}");
-            var s = "[]{}()()";
-            var res = IsValidParentheses(s);
-            Console.WriteLine(res);
+            // var s = "[]{}()()";
+            // var res = IsValidParentheses(s);
+            // Console.WriteLine(res);
+
+            int[] arr1 = new int[] { 1, 2, 3, 4 };
+            int[] arr2 = new int[] { 10, 20, 30, 40 };
+
+            Console.WriteLine($"{string.Join(",",MergeTwoSortedArray(arr1, arr2))}");
+
 
         }
 
@@ -58,26 +64,73 @@ namespace StandardProblems.Problems
         {
             Stack<char> chars = new Stack<char>();
             foreach (char c in s)
+            {
+                if (c == '{' || c == '[' || c == '(')
                 {
-                    if (c == '{' || c == '[' || c == '(')
-                    {
-                        chars.Push(c);
-                    }
-                    
-                    else if (c == ')' || c == ']' || c == '}')
-                    {
-                        if (chars.Count == 0) return false; // پشته خالی است، پس نامعتبر
-                        char top = chars.Pop();
-                        if ((c == ')' && top != '(') ||
-                            (c == ']' && top != '[') ||
-                            (c == '}' && top != '{'))
-                        {
-                            return false; // پرانتزها مطابقت ندارند
-                        }
-                    }
-                    
+                    chars.Push(c);
                 }
-            return chars.Count==0;
+
+                else if (c == ')' || c == ']' || c == '}')
+                {
+                    if (chars.Count == 0) return false; // پشته خالی است، پس نامعتبر
+                    char top = chars.Pop();
+                    if ((c == ')' && top != '(') ||
+                        (c == ']' && top != '[') ||
+                        (c == '}' && top != '{'))
+                    {
+                        return false; // پرانتزها مطابقت ندارند
+                    }
+                }
+
+            }
+            return chars.Count == 0;
+        }
+        
+        public static int[] MergeTwoSortedArray(int[] arr1,int[] arr2)
+        {
+           
+            
+            int n1 = arr1.Length;
+            int n2 = arr2.Length;
+
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
+
+            int n3 = n1 + n2;
+            int[] result = new int[n1+n2];
+
+            while (p1 < n1 && p2 < n2)
+            {
+                if (arr1[p1] <= arr2[p2])
+                {
+                    result[p3] = arr1[p1];
+                    p1++;
+                }
+                else
+                {
+                    result[p3] = arr2[p2];
+                    p2++;
+                }
+
+                p3++;
+            }
+
+            while (p1 < n1)
+            {
+                result[p3] = arr1[p1];
+                p3++;
+                p1++;
+            }
+
+            while (p2 < n2)
+            {
+                result[p3] = arr2[p2];
+                p3++;
+                p2++;
+            }
+
+            return result;
         }
     }
 }
