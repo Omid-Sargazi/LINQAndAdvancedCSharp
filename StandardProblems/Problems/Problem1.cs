f43a4c1ca857ba33a59af97510970a7c7e595df8
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-03 13:49:20 +0330

    Is Valid Paranteses

--- StandardProblems/Problems/Problem1.cs
+++ StandardProblems/Problems/Problem1.cs
@@ -4,8 +4,11 @@ namespace StandardProblems.Problems
     {
         public static void Run()
         {
-            var res = SumTwoNums(new int[] { 1, 2, 3, 4, 6, 8, 9, 11 }, 10);
-            Console.WriteLine($"Indices: {res[0]}, {res[1]}");
+            // var res = SumTwoNumsAnotherWay(new int[] { 1, 2, 3, 4, 6, 8, 9, 11 }, 10);
+            // Console.WriteLine($"Indices: {res[0]}, {res[1]}");
+            var s = "[]{}()()";
+            var res = IsValidParentheses(s);
+            Console.WriteLine(res);
 
         }
 
@@ -33,9 +36,48 @@ namespace StandardProblems.Problems
             return new int[] { -1, -1 };
         }
 
-        public static bool SumTwoNumsAnotherWay(int[] nums, int target)
+        public static int[] SumTwoNumsAnotherWay(int[] nums, int target)
         {
-            
+            var n = nums.Length;
+            var seen = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int completed = target - nums[i];
+                if (seen.ContainsKey(completed))
+                {
+                    return new int[] { seen[completed], i };
+                }
+                seen[nums[i]] = i;
+            }
+
+            return new int[] { -1, -1 };
+        }
+
+        public static bool IsValidParentheses(string s)
+        {
+            Stack<char> chars = new Stack<char>();
+            foreach (char c in s)
+                {
+                    if (c == '{' || c == '[' || c == '(')
+                    {
+                        chars.Push(c);
+                    }
+                    
+                    else if (c == ')' || c == ']' || c == '}')
+                    {
+                        if (chars.Count == 0) return false; // پشته خالی است، پس نامعتبر
+                        char top = chars.Pop();
+                        if ((c == ')' && top != '(') ||
+                            (c == ']' && top != '[') ||
+                            (c == '}' && top != '{'))
+                        {
+                            return false; // پرانتزها مطابقت ندارند
+                        }
+                    }
+                    
+                }
+            return chars.Count==0;
         }
     }
 }
