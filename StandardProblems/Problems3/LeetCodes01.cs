5c3c3db2b32996777820c7b3c83dbadccbbc2c04
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-04 21:14:36 +0330

    Max Subarray in an array

--- StandardProblems/Problems3/LeetCodes01.cs
+++ StandardProblems/Problems3/LeetCodes01.cs
@@ -19,5 +19,23 @@ namespace StandardProblems.Problems3
             }
             return maxProfit;
         }
+
+        public static bool Palindrome(string s)
+        {
+            int left = 0;int right = s.Length - 1;
+
+            while (left < right)
+            {
+                while (left < right && char.IsLetterOrDigit(s[left])) left++;
+                while (left < right && char.IsLetterOrDigit(s[right])) right--;
+
+                if (char.ToLower(s[left]) != char.ToLower(s[right])) return false;
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
     }
 }
--- StandardProblems/Program.cs
+++ StandardProblems/Program.cs
@@ -12,4 +12,6 @@ Console.WriteLine("Hello, World!");
 
 var nums = new int[] { 7, 6, 4, 3, 1 };
 var nums2 = new int[] { 7,1,5,3,6,4 };
-Console.WriteLine(LeetCodes01.MaxProfit(nums2));
+// Console.WriteLine(LeetCodes01.MaxProfit(nums2));
+var nums3 = new int[] { 1, 1, 2, 2, 3, 4, 4, 5, 6, 7, 8 };
+LeetCodes01.RemoveDuplicateFromSortedArray(nums3);
