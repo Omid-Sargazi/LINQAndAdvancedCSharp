d83ff0aa6d1bbad45a3072c901ed92aa176d0b20
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-05 20:54:36 +0330

    Bridge Pattern

--- DIPattern/OOPDesign/FileSystem.cs
+++ DIPattern/OOPDesign/FileSystem.cs
@@ -130,4 +130,101 @@ ErrorMessage="401",
         public string TransactionId { get; set; }
         public string ErrorMessage { get; set; }
     }
+    //////////////////////////////////////////////////////
+    /// 
+    /// 
+
+
+    public interface IReportingSystem
+    {
+        void MakeReport(string context);
+    }
+    public class PdfReport : IReportingSystem
+    {
+        public void MakeReport(string context)
+        {
+            Console.WriteLine("PDF Report is Created");
+        }
+    }
+
+    public class ExeclReport : IReportingSystem
+    {
+        public void MakeReport(string context)
+        {
+            Console.WriteLine("Excel Report is Created");
+        }
+    }
+    public class HTMLReport : IReportingSystem
+    {
+        public void MakeReport(string context)
+        {
+            Console.WriteLine("HTML Report is Created");
+        }
+    }
+
+
+
+
+
+    public abstract class ReportingSystem
+    {
+        protected  IReportingSystem _reportingSystem;
+        public ReportingSystem(IReportingSystem reportingSystem)
+        {
+            _reportingSystem = reportingSystem;
+        }
+
+        public abstract void Send(string context);
+    }
+    public class FaxSendReport : ReportingSystem
+    {
+        public FaxSendReport(IReportingSystem reportingSystem) : base(reportingSystem)
+        {
+        }
+
+        public override void Send(string context)
+        {
+            Console.Write("Sending via Fax - ");
+            _reportingSystem.MakeReport(context);
+            
+        }
+    }
+
+    public class EmailSendReport : ReportingSystem
+    {
+        public EmailSendReport(IReportingSystem reportingSystem) : base(reportingSystem)
+        {
+        }
+
+        public override void Send(string context)
+        {
+            Console.Write("Sending via Email - ");
+            _reportingSystem.MakeReport(context);
+        }
+    }
+
+    public class PrinterSendReport : ReportingSystem
+    {
+        public PrinterSendReport(IReportingSystem reportingSystem) : base(reportingSystem)
+        {
+        }
+
+        public override void Send(string context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class ClientBridgePattern
+    {
+        public static void Execute()
+        {
+            IReportingSystem makeReport = new HTMLReport();
+            makeReport.MakeReport("A report about last task");
+
+            EmailSendReport emailSend = new EmailSendReport(makeReport);
+            emailSend.Send("Omid");
+        }
+    }
 }
