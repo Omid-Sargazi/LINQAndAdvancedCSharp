69f9a3e27b74d22517397d9e00594b65bf30bbe9
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-05 13:11:35 +0330

    Apply Interface sigrigation

--- DIPattern/OOPDesign/FileSystem.cs
+++ DIPattern/OOPDesign/FileSystem.cs
@@ -1,10 +1,8 @@
 namespace DIPattern.OOPDesign
 {
-    public interface IFileSystem
+    public interface IFileSystem:IFileDeletable,IFileOpenable,IFileSavable
     {
-        void Open();
-        void Delete();
-        void Save();
+        
     }
 
     public class AudioFile : IFileSystem
@@ -41,5 +39,33 @@ namespace DIPattern.OOPDesign
         {
             throw new NotImplementedException();
         }
+    }
+
+    public class ReadOnlyConfigFile : IFileOpenable, IFileDeletable
+    {
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IFileOpenable
+    {
+        void Open();
+    }
+
+    public interface IFileSavable
+    {
+        void Save();
+    }
+
+    public interface IFileDeletable
+    {
+        void Delete();
     }
 }
