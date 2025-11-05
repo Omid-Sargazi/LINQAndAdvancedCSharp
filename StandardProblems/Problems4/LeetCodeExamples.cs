e8699091de414eac68a3f92a9ab0b70c2dba981f
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-05 09:42:39 +0330

    .

--- StandardProblems/Problems4/LeetCodeExamples.cs
+++ StandardProblems/Problems4/LeetCodeExamples.cs
@@ -29,5 +29,40 @@ namespace StandardProblems.Problems4
 
             Console.WriteLine($"MaximumProfit: {maxProfit}");
         }
+
+        public static void FindMaxNumber(int[] arr, int k)
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                max = Math.Max(arr[i], max);
+            }
+
+            Console.WriteLine($"Max Number in an array:{max}");
+        }
+
+        public static void MaxSumAllPositive(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+        }
+        
+        public static void MaxSubarraySum(int[] arr)
+        {
+            int maxSofar = arr[0];
+            int currentMax = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                currentMax = Math.Max(arr[i], arr[i] + currentMax);
+                maxSofar = Math.Max(maxSofar, currentMax);
+            }
+            
+            Console.WriteLine("Maximum Subarray Sum = " + maxSofar);
+        }
     }
 }
