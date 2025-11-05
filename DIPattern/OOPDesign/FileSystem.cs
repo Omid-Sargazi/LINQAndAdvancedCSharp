17f1e675f8a170e5d9f0e3fb451dabdc4bf9987d
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-05 13:38:39 +0330

    Dependancy injection Pattern

--- DIPattern/OOPDesign/FileSystem.cs
+++ DIPattern/OOPDesign/FileSystem.cs
@@ -68,4 +68,66 @@ namespace DIPattern.OOPDesign
     {
         void Delete();
     }
+
+
+    public interface IPayamnt
+    {
+        PaymentResult ProcessPayment(decimal amount);
+    }
+
+    public class PayPal : IPayamnt
+    {
+        public PaymentResult ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing ${amount} via PayPal...");
+            return new PaymentResult
+            {
+                IsSuccess = true,
+                TransactionId="Pay"+Guid.NewGuid
+            };
+        }
+    }
+
+    public class Stripe : IPayamnt
+    {
+        public PaymentResult ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing ${amount} via Stripe...");
+
+            return new PaymentResult
+            {
+                IsSuccess = false,
+ErrorMessage="401",
+            };
+        }
+    }
+
+    public class BankTransfer : IPayamnt
+    {
+        public PaymentResult ProcessPayment(decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PaymentProcessingSystem
+    {
+        private readonly IPayamnt _payamnt;
+        public PaymentProcessingSystem(IPayamnt payamnt)
+        {
+            _payamnt = payamnt;
+        }
+
+        public void Process(decimal amount)
+        {
+            _payamnt.ProcessPayment(amount);
+        }
+    }
+
+    public class PaymentResult
+    {
+        public bool IsSuccess { get; set; }
+        public string TransactionId { get; set; }
+        public string ErrorMessage { get; set; }
+    }
 }
