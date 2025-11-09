00138b3dddc29af0c9986046732be65a9280936f
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-09 18:22:06 +0330

    .

--- LinqExamples/ExamplesOfLinq/SELECT CategoryName,AVG(UnitPrice) as Av.sql
+++ LinqExamples/ExamplesOfLinq/SELECT CategoryName,AVG(UnitPrice) as Av.sql
@@ -1,1 +1,5 @@
-SELECT CategoryName,AVG(UnitPrice) as AvePrice FROM Products  as p JOIN Categories as c on p.CategoryID = c.CategoryID GROUP BY CategoryName HAVING AVG(UnitPrice)>28
+-- SELECT CategoryName,AVG(UnitPrice) as AvePrice FROM Products  as p JOIN Categories as c on p.CategoryID = c.CategoryID GROUP BY CategoryName HAVING AVG(UnitPrice)>28
+SELECT CategoryName,COUNT(*) NumbersOfCategories FROM Products as p JOIN Categories as c on p.CategoryID = c.CategoryID GROUP BY CategoryName
+
+SELECT City,COUNT(CustomerID) NumbersOfCustomer FROM Customers GROUP BY City HAVING COUNT(CustomerID)>2
+
