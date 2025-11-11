4ca092a30eb75ddcc3f749ca3fdf6299a0f2e8c7
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-11 21:55:35 +0330

    Group employees by department and calculate statistics

--- LinqProblems/Problems01/LinqProblems01.cs
+++ LinqProblems/Problems01/LinqProblems01.cs
@@ -1,6 +1,6 @@
 namespace LinqProblems.Problems01
 {
-    public class LinqProblems
+    public class LinqProblems01
     {
         public static void Execute()
         {
@@ -14,7 +14,17 @@ namespace LinqProblems.Problems01
         };
             var average = employees.Average(e => e.Salary);
 
-            var res1 = employees.Where(e => e.Salary > employees.Average(e => e.Salary)).Select(e=>new{e.Name,e.Salary});
+            var res1 = employees.Where(e => e.Salary > employees.Average(e => e.Salary)).Select(e => new { e.Name, e.Salary });
+
+            var res2 = employees.GroupBy(e => e.Department).Select(g => new
+            {
+                DepName = g.Key,
+                NumsOfEmployee = g.Count(),
+                AveSalary = g.Average(e => e.Salary),
+                highSalary = g.Max(e=>e.Salary)
+            });
+
+            Console.WriteLine($"{string.Join(",",res2.Select(e=>e.DepName))}");
 
         }
     }
--- LinqProblems/Program.cs
+++ LinqProblems/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LinqProblems.Problems01;
+
 Console.WriteLine("Hello, World!");
+LinqProblems01.Execute();
