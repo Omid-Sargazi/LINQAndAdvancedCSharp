3cd828b1c232175316d277c2267e8e69c1e4e834
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-13 11:40:35 +0330

    .

--- AdventureWorksLINQ.Console/LinqProblems/QueryInAdventireWorks2019.sql
+++ AdventureWorksLINQ.Console/LinqProblems/QueryInAdventireWorks2019.sql
@@ -5,5 +5,9 @@
 -- INNER JOIN Person.Person p ON e.BusinessEntityID=p.BusinessEntityID
 -- INNER JOIN HumanResources.EmployeeDepartmentHistory edh ON e.BusinessEntityID=edh.BusinessEntityID
 -- INNER JOIN HumanResources.Department d ON edh.DepartmentID=d.DepartmentID WHERE d.Name = 'Research and Development'
-SELECT p.LastName+''+p.FirstName as FullName,COUNT(soh.SalesOrderID) as orderCount from Person.Person as p JOIN Sales.SalesOrderHeader as soh on p.BusinessEntityID=soh.CustomerID WHERE soh.OrderDate>='2014-01-01' AND soh.OrderDate<'2015-01-01'
-GROUP BY p.LastName,p.FirstName ORDER BY orderCount DESC
+-- SELECT p.LastName+''+p.FirstName as FullName,COUNT(soh.SalesOrderID) as orderCount from Person.Person as p JOIN Sales.SalesOrderHeader as soh on p.BusinessEntityID=soh.CustomerID WHERE soh.OrderDate>='2014-01-01' AND soh.OrderDate<'2015-01-01'
+-- GROUP BY p.LastName,p.FirstName ORDER BY orderCount DESC
+
+SELECT p.Name,p.ProductNumber,SUM(sod.OrderQty)as TotalSold from Production.Product as p INNER JOIN Sales.SalesOrderDetail as sod on p.ProductID = sod.ProductID INNER JOIN Sales.SalesOrderHeader as soh 
+on soh.SalesOrderID = sod.SalesOrderID WHERE soh.OrderDate>='2013-01-01' AND soh.OrderDate<='2014-01-01' GROUP BY p.Name,p.ProductNumber HAVING SUM(sod.OrderQty)>=100
+ORDER BY TotalSold DESC;
