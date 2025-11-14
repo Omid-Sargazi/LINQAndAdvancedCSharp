acaa3b9aabd54aa2c1b5342ff7dfe244e4c111f9
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-14 12:38:44 +0330

    .

--- AdventureWorksLINQ.Console/QueryExample/Queries.cs
+++ AdventureWorksLINQ.Console/QueryExample/Queries.cs
@@ -31,14 +31,14 @@ public class Queries
         }
 
         var query20 = from d in db.SalesOrderDetails join h in db.SalesOrderHeaders on
-        d.SalesOrderId equals h.SalesOrderId join p in db.Products d.ProductId equals p.ProductId where h.Status == 5
-                      group d by new { p.ProductId, p.Name } into g orderby g.Sum(x => x.LineTotal) descending select new
+        d.SalesOrderId equals h.SalesOrderId join prod in db.Products on d.ProductId equals prod.ProductId where h.Status == 5
+                      group d by new { prod.ProductId, prod.Name } into g orderby g.Sum(x => x.LineTotal) descending select new
                       {
                           ProductId = g.Key.ProductId,
                           ProductName = g.Key.Name,
                           TotalSale = g.Sum(x => x.LineTotal)
                       };
-                      
+
     }
     
    
