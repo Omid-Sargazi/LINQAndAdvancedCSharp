f802e1f1b2091708e3aa62598f70af01c76110c0
Omid-Sargazi <om.sargazi@gmail.com>
2025-11-21 00:58:46 +0330

    Login,Logout and Confirm Email with Identity Asp.net Core

--- AspNetCoreIdentityDemo/WebApp1/Data/Migrations/20251120205617_InitialIdentity.Designer.cs
+++ AspNetCoreIdentityDemo/WebApp1/Data/Migrations/20251120205617_InitialIdentity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp1.Data;
 
@@ -10,9 +11,11 @@ using WebApp1.Data;
 namespace WebApp1.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251120205617_InitialIdentity")]
+    partial class InitialIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0-rc.2.24474.1");
--- AspNetCoreIdentityDemo/WebApp1/Program.cs
+++ AspNetCoreIdentityDemo/WebApp1/Program.cs
@@ -5,14 +5,50 @@ using WebApp1.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                       ?? "Data Source=app.db";
+
+                       builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString)); 
+
+
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
+
+    builder.Services.AddRazorPages();
+
+    builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;                      // امنیت بالاتر: JS نمی‌تونه کوکی رو بخونه
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);    // مدت اعتبار کوکی
+    options.LoginPath = "/Identity/Account/Login";       // اگر [Authorize] و کاربر لاگین نباشه → اینجا
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied"; // اگر لاگین هست ولی دسترسی نداره
+    options.SlidingExpiration = true;                    // اگر کاربر فعال باشه، زمان اعتبار تمدید می‌شه
+});
+
 
 var app = builder.Build();
 
